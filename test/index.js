d23f033023edff66380fa966d1f3cb9ff3455b8a
Song An Bui <songan.bui@thomsonreuters.com>
2016-02-02 11:50:10 +0100

    add job core properties validation

--- test/index.js
+++ test/index.js
@@ -23,82 +23,82 @@ describe('Module loading', function() {
   });
 });
 
-//describe('Brick - validate job', function() {
-//  context('when missing/incorrect \'id\' string property in job', function() {
-//    it('should throw an error', function() {
-//      const job = {};
-//      const validatePromise = brick.validate(job);
-//      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'id\' string property in job');
-//    });
-//  });
-//
-//  context('when missing/incorrect \'nature\' object property in job', function() {
-//    it('should throw an error', function() {
-//      const job = {
-//        id: '001',
-//      };
-//      const validatePromise = brick.validate(job);
-//      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'nature\' object property in job');
-//    });
-//  });
-//
-//  context('when missing/incorrect \'quality\' string property in job nature', function() {
-//    it('should throw an error', function() {
-//      const job = {
-//        id: '001',
-//        nature: {
-//          type: 'bar',
-//        },
-//      };
-//      const validatePromise = brick.validate(job);
-//      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'quality\' string property in job nature');
-//    });
-//  });
-//
-//  context('when missing/incorrect \'type\' string property in job nature', function() {
-//    it('should throw an error', function() {
-//      const job = {
-//        id: '001',
-//        nature: {
-//          quality: 'Execution',
-//        },
-//      };
-//      const validatePromise = brick.validate(job);
-//      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'type\' string property in job nature');
-//    });
-//  });
-//
-//  context('when missing/incorrect \'payload\' object property in job', function() {
-//    it('should throw an error', function() {
-//      const job = {
-//        id: '001',
-//        nature: {
-//          quality: 'Execution',
-//          type: 'CommandLine',
-//        },
-//      };
-//      const validatePromise = brick.validate(job);
-//      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'payload\' object property in job');
-//    });
-//  });
-//
-//  context('when job is valid', function() {
-//    it('should throw an error', function(done) {
-//      const job = {
-//        id: '001',
-//        nature: {
-//          quality: 'Execution',
-//          type: 'CommandLine',
-//        },
-//        payload: {},
-//      };
-//      const validatePromise = brick.validate(job);
-//      expect(validatePromise).to.eventually.be.an('object')
-//        .and.to.have.property('ok', 1);
-//      done();
-//    });
-//  });
-//});
+describe('Brick - validate job', function() {
+  context('when missing/incorrect \'id\' string property in job', function() {
+    it('should throw an error', function() {
+      const job = {};
+      const validatePromise = brick.validate(job);
+      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'id\' string property in job');
+    });
+  });
+
+  context('when missing/incorrect \'nature\' object property in job', function() {
+    it('should throw an error', function() {
+      const job = {
+        id: '001',
+      };
+      const validatePromise = brick.validate(job);
+      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'nature\' object property in job');
+    });
+  });
+
+  context('when missing/incorrect \'quality\' string property in job nature', function() {
+    it('should throw an error', function() {
+      const job = {
+        id: '001',
+        nature: {
+          type: 'bar',
+        },
+      };
+      const validatePromise = brick.validate(job);
+      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'quality\' string property in job nature');
+    });
+  });
+
+  context('when missing/incorrect \'type\' string property in job nature', function() {
+    it('should throw an error', function() {
+      const job = {
+        id: '001',
+        nature: {
+          quality: 'Execution',
+        },
+      };
+      const validatePromise = brick.validate(job);
+      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'type\' string property in job nature');
+    });
+  });
+
+  context('when missing/incorrect \'payload\' object property in job', function() {
+    it('should throw an error', function() {
+      const job = {
+        id: '001',
+        nature: {
+          quality: 'Execution',
+          type: 'CommandLine',
+        },
+      };
+      const validatePromise = brick.validate(job);
+      return expect(validatePromise).to.eventually.be.rejectedWith(Error, 'missing/incorrect \'payload\' object property in job');
+    });
+  });
+
+  context('when job is valid', function() {
+    it('should throw an error', function(done) {
+      const job = {
+        id: '001',
+        nature: {
+          quality: 'Execution',
+          type: 'CommandLine',
+        },
+        payload: {},
+      };
+      const validatePromise = brick.validate(job);
+      expect(validatePromise).to.eventually.be.an('object')
+        .and.to.have.property('ok', 1);
+      done();
+    });
+  });
+});
 
 describe('Brick - do job', function() {
   it('should fulfill promise', function() {
